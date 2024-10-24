ea58e595ae7deb0c762ba68e1daf91ec8b6dbb77
kerlous0 <kerlousnasser0@gmail.com>
2024-10-25 00:11:59 +0300

    try18

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use("/thumbnail", thumbnail);
 app.use("/download", download);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
